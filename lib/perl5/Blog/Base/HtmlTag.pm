6b73ee58d158433bf10f6c7f4f140a11d555f848
s31tz <fs@fseitz.de>
2015-07-24 17:04:12 +0200

    * styleSheet= entfernt * css hinzugefügt * image hinzugefügt

--- lib/perl5/Blog/Base/HtmlTag.pm
+++ lib/perl5/Blog/Base/HtmlTag.pm
@@ -1215,6 +1215,7 @@ sub tag {
             }
             else {
                 if ($embedImages && $tag eq 'img' && $key eq 'src') {
+warn "$val\n";
                     if (-f $val) {
                         # Bette Bilddaten in das src-Attribut ein
                         # FIXME: Bild-URLs auch laden
