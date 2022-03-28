f269cd12a2b0b16fc62fa9f3787c0d77bf1fab15
Frank Seitz <fs@fseitz.de>
2022-03-28 07:53:23 +0200

    Savepoint

--- lib/perl5/Blog/Base/Quiq/Path.pm
+++ lib/perl5/Blog/Base/Quiq/Path.pm
@@ -31,7 +31,7 @@ use strict;
 use warnings;
 use utf8;
 
-our $VERSION = '1.201';
+our $VERSION = '1.202';
 
 use Blog::Base::Quiq::Option;
 use Blog::Base::Quiq::FileHandle;
@@ -3713,7 +3713,7 @@ sub uid {
 
 =head1 VERSION
 
-1.201
+1.202
 
 =head1 AUTHOR
 
