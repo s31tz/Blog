63f85f50329498d83304c9a56a7998d444a959d8
Frank Seitz <fs@fseitz.de>
2023-11-19 18:00:06 +0100

    Savepoint

--- lib/perl5/Blog/Base/Quiq/Table.pm
+++ lib/perl5/Blog/Base/Quiq/Table.pm
@@ -95,7 +95,7 @@ use v5.10;
 use strict;
 use warnings;
 
-our $VERSION = '1.212';
+our $VERSION = '1.213';
 
 use Blog::Base::Quiq::Hash;
 use Blog::Base::Quiq::Properties;
@@ -786,7 +786,7 @@ sub asText {
 
 =head1 VERSION
 
-1.212
+1.213
 
 =head1 AUTHOR
 
