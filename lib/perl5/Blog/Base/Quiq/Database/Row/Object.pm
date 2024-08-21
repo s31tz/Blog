6ed2f8013c1e1b9bbd8ec699729d3224262b95e7
Frank Seitz <fs@fseitz.de>
2024-08-21 09:00:08 +0200

    Savepoint

--- lib/perl5/Blog/Base/Quiq/Database/Row/Object.pm
+++ lib/perl5/Blog/Base/Quiq/Database/Row/Object.pm
@@ -26,7 +26,7 @@ use strict;
 use warnings;
 use utf8;
 
-our $VERSION = '1.218';
+our $VERSION = '1.219';
 
 use Blog::Base::Quiq::Hash;
 use Blog::Base::Quiq::Option;
@@ -1657,7 +1657,7 @@ sub AUTOLOAD {
 
 =head1 VERSION
 
-1.218
+1.219
 
 =head1 AUTHOR
 
--- lib/perl5/Blog/Base/Quiq/Sql.pm
+++ lib/perl5/Blog/Base/Quiq/Sql.pm
@@ -68,7 +68,7 @@ use strict;
 use warnings;
 use utf8;
 
-our $VERSION = '1.218';
+our $VERSION = '1.219';
 
 use Blog::Base::Quiq::Hash;
 use Blog::Base::Quiq::Option;
@@ -5694,7 +5694,7 @@ sub diff {
 
 =head1 VERSION
 
-1.218
+1.219
 
 =head1 AUTHOR
 
