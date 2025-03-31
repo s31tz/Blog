f8f886cf85f95949e9d0c545bdcb259af20f239e
Frank Seitz <fs@fseitz.de>
2025-03-31 12:00:08 +0200

    Savepoint

--- lib/perl5/Blog/Base/Quiq/PlotlyJs/TimeSeries.pm
+++ lib/perl5/Blog/Base/Quiq/PlotlyJs/TimeSeries.pm
@@ -236,7 +236,7 @@ use v5.10;
 use strict;
 use warnings;
 
-our $VERSION = '1.224';
+our $VERSION = '1.225';
 
 use Blog::Base::Quiq::Json::Code;
 use Blog::Base::Quiq::Template;
@@ -637,7 +637,7 @@ sub js {
 
 =head1 VERSION
 
-1.224
+1.225
 
 =head1 AUTHOR
 
