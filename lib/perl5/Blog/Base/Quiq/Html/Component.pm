93d0e0c95a729ecfa557350729e40c2bebb0c1dd
Frank Seitz <fs@fseitz.de>
2026-04-19 20:00:08 +0200

    Savepoint

--- lib/perl5/Blog/Base/Quiq/Html/Component.pm
+++ lib/perl5/Blog/Base/Quiq/Html/Component.pm
@@ -68,7 +68,7 @@ use v5.10;
 use strict;
 use warnings;
 
-our $VERSION = '1.236';
+our $VERSION = '1.237';
 
 use Blog::Base::Quiq::Css;
 use Blog::Base::Quiq::JavaScript;
@@ -407,7 +407,7 @@ sub putValue {
 
 =head1 VERSION
 
-1.236
+1.237
 
 =head1 AUTHOR
 
